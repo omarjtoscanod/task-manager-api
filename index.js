58a70fdd001b2650024492244d2202633dd45f15
Omar Toscano <otoscano@uninorte.edu.co>
2022-05-01 15:07:52 -0500

    Create connection to MongoDB database

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const http = require("http");
 
 const app = require("./server");
 const config = require("./server/config");
+const database = require("./server/database");
 
-const { port } = config;
+// Connect to databse
+database.connect(config.database);
 
+// create Web Server
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server running at ${port}`);
+server.listen(config.port, () => {
+  console.log(`Server running at ${config.port}`);
 });
--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 
 const config = {
   port: process.env.port,
+  database: {
+    protocol: process.env.DB_PROTOCOL,
+    url: process.env.DB_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
 };
 
 module.exports = config;
